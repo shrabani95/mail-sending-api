abcfba6663aa6fd33d256160c4dba74cba21b962
shrabani95 <sarkar.shrabani@yahoo.in>
2020-12-30 00:20:34 +0530

    html mail

--- index.js
+++ index.js
@@ -20,11 +20,14 @@ const transporter = nodemailer.createTransport({
           pass: 'oagurmmdihqobqsq' // naturally, replace both with your real credentials or an application-specific password
         }
       });   
+
+ var mailContent = '<!DOCTYPE HTML><head><meta http-equiv="content-type" content="text/html"></head><body><div id="outer" style="width: 80%;margin: 0 auto;margin-top: 10px;"><div id="inner" style="width: 78%;margin: 0 auto;background-color: #fff;font-family: Open Sans,Arial,sans-serif;font-size: 13px;font-weight: normal;line-height: 1.4em;color: #444;margin-top: 10px;"><p>Name : '+
+  data.name+'</p><p>Email: ' + data.email+'</p><p>Phone: ' + data.phone+'</p><p>Subject: ' + data.subject +' </p><p>Message : ' + data.message+'</p></div></div></body>';
       const mailOptions = { 
         from: 'shrabanis8@gmail.com',
         to: data.to, 
-       subject: data.subject,
-        html : data.message,
+       subject: 'Contact Form',
+        html : mailContent,
 
       };   
       const result =  await transporter.sendMail(mailOptions);
